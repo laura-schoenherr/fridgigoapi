e8a7bccb3ef3241d3a7cd9b2593a7e45c5696c11
laura-schoenherr <lauraschoenherr@outlook.de>
2022-02-24 02:58:17 +0100

    Initial commit

---
+++ src/test/java/com/example/fridgigoapi/FridgigoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fridgigoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgigoapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
